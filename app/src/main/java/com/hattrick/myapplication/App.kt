2518f05a393a043a94d2b619ba1535e9b8be7d74
Marcelo Seoane <marcelo@hattrick-it.com>
2022-04-27 16:35:32 -0300

    ReleaseTree class created

--- app/src/main/java/com/hattrick/myapplication/App.kt
+++ app/src/main/java/com/hattrick/myapplication/App.kt
@@ -2,6 +2,7 @@ package com.hattrick.myapplication
 
 import android.app.Application
 import com.hattrick.myapplication.di.appModule
+import com.hattrick.myapplication.util.ReleaseTree
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -17,6 +18,10 @@ class App: Application() {
             modules(appModule)
         }
 
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(ReleaseTree())
+        }
     }
 }
